2061ad3a0e75d4c006bb801fc52680a9a5107856
yurididomenico <yurididomenico93@gmail.com>
2022-10-28 15:13:30 +0200

    JS: Definizione funzione al click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,20 @@
 // JAVASCRIPT //
-
 console.log("YDD");
 
+/*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
 
+function gioco()
+{
+    console.log("Funzione: gioco()")
+
+
+    
+}
 
 
 
